477a1710d724813422c41e57771a74808d856790
Greg Martin <gregory.martin.data@gmail.com>
2026-09-11 08:23:10 +0200

    style(campaign): mark __exit__ exc_info as intentionally unused

--- src/valid_tva/campaign.py
+++ src/valid_tva/campaign.py
@@ -166,7 +166,7 @@ class ViesClient:
         """Enter: the client is already open."""
         return self
 
-    def __exit__(self, *exc_info: object) -> None:
+    def __exit__(self, *_exc_info: object) -> None:
         """Close the underlying HTTP client."""
         self._client.close()
 
